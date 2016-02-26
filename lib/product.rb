5fbd27df151baa02e39637601e070a960d936bd2
Katerina <kbaneva@gmail.com>
2016-02-26 15:03:28 +0200

    Add ability to raise an error if the product already exists in the products list

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,23 @@
 class Product
+	attr_reader :title
 	@@products = []
 
 	def initialize(options={})
-		@@products << self
+		@title = options[:title]
+		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	private
+
+	def add_to_products
+		unless (@@products.any? { |product| product.title == title})
+			@@products << self
+		else	
+			raise DuplicateProductError, " '#{title}' already exists."
+	    end
 	end
 end
